a2bc47e70cbcd4a7bf0d67378c83a613ae159c8f
Jimmy Jardland <jimmy@jardland.se>
2021-03-01 22:08:06 +0100

    fix: ios statusbar and gradient

--- components/games.component.js
+++ components/games.component.js
@@ -7,17 +7,17 @@ import { ScrollView } from 'react-native-gesture-handler'
 
 export const Games = ({ navigation }) => (
   <>
-    <SafeAreaView>
       <LinearGradient
         start={{ x: 0, y: 0.4385191355532845 }}
         end={{ x: 1, y: 0.5614808644467155 }}
         colors={['#140933', '#2A146A']}
       >
-        <ScrollView>
-          <Logo style={{ marginTop: 40, marginLeft: 30, marginBottom: 10 }} />
-          <GameList navigation={navigation} />
-        </ScrollView>
+        <SafeAreaView>
+            <ScrollView>
+              <Logo style={{ marginTop: 40, marginLeft: 30, marginBottom: 10 }} />
+              <GameList navigation={navigation} />
+            </ScrollView>
+        </SafeAreaView>
       </LinearGradient>
-    </SafeAreaView>
   </>
 )
